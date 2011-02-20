c7068f8de6ae7f9eda76364a1906da5e45403046
B V Satyaram <bvsatyaram@gmail.com>
2011-02-21 00:42:34 +0530

    MultiDimensionalArray

---
+++ lib/multi_dimensional_array.rb
@@ -0,0 +1,42 @@
+class MultiDimensionalArray
+  def initialize(*dimensions)
+    @dimensions = Array.new(dimensions.length)
+    @factors = Array.new(dimensions.length)
+    product = 1
+    i = dimensions.length - 1
+
+    while i >= 0
+      @dimensions[i] = dimensions[i]
+      @factors[i] = product
+      product *= dimensions[i]
+      i -= 1
+    end
+
+    @data = Array.new(product)
+  end
+
+  def get_offset(indices)
+    raise IndexError if indices.length != @dimensions.length
+
+    offset = 0
+
+    (0..(@dimensions.length - 1)).each do |i|
+      if indices[i] < 0 || indices[i] >= @dimensions[i]
+        raise IndexError
+      end
+
+      offset += @factors[i]*indices[i]
+    end
+
+    return offset
+  end
+
+  def [](*indices)
+    @data[self.get_offset(indices)]
+  end
+
+  def []=(*indices_and_value)
+    value = indices_and_value.pop
+    @data[self.get_offset(indices_and_value)] = value
+  end
+end
---
+++ test/multi_dimensional_array_test.rb
@@ -0,0 +1,17 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'multi_dimensional_array'
+
+class MultiDimensionalArrayTest < Test::Unit::TestCase
+  def test_multi_dimensional_array
+    mda = MultiDimensionalArray.new(2,3,4)
+    assert_equal nil, mda[1,2,3]
+
+    mda[1,2,3] = 5
+    assert_equal 5, mda[1,2,3]
+  end
+end
